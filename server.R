860e112cede0dcf6beb4f6e85785fe208621784d
nsmader <nsmader@gmail.com>
2015-07-12 18:48:37 -0500

    More debugging to fit ui and new server files together

--- server.R
+++ server.R
@@ -17,105 +17,138 @@ source("./power-fns.R")
 
 shinyServer(function(input, output){
   
-  Results <- reactive({
-    
-    # Isolate all of the following assignments to not allow them to run until the "Run Sim" button is clicked
-    # Great description of isolation: http://shiny.rstudio.com/articles/isolation.html
-    alpha  = reactive(as.numeric(input$alpha))
-    resid.var = reactive(as.numeric(input$resid.var))
-    
-    # Make modifications if not using a clustered design
-    if (input$clusterDesign == T){
-      cluster.ICC = reactive(as.numeric(input$cluster.ICC))
-      cluster.var = ( cluster.ICC*pi^2/3)/(1-cluster.ICC)
+  alpha  = reactive(as.numeric(input$alpha))
+  resid.var = reactive(as.numeric(input$resid.var))
+  
+  # Make modifications if not using a clustered design
+  if (input$clusterDesign == T){
+    cluster.ICC = reactive(as.numeric(input$cluster.ICC))
+    cluster.var = ( cluster.ICC*pi^2/3)/(1-cluster.ICC)
 #       cluster.num  = isolate(as.integer(input$cluster.num))
 #       cluster.size = isolate(as.integer(input$cluster.size))
-    } else {
-      cluster.num <- 1
-      cluster.ICC <- 0
-    }
-    
-    if (input$outcomeType == "cts") {
+  } else {
+    cluster.num <- 1
+    cluster.ICC <- 0
+    cluster.var <- 0
+  }
+  
+  if (input$outcomeType == "cts"){
 
-      if (input$clusterDesign == T){
-        
-        # 1. specify power and effect size --> receive cluster size vs. # clusters (discrete)
-          # /!\ Minor note--could redo this sequence of if()s to be a switch()
-          if (input$clusterRequest == "cluster size vs. # clusters"){
-            power       <- reactive(as.numeric(input$power))
-            effect.size <- reactive(as.numeric(input$effect.size))
-            clusterNums = 1:50
-            #plotTable <- data.frame(clusterNums, clusterSize = ctsClus_getClusterSize_clusterNum ( clusterNums ))
-              # XXX Not yet defined            
-            # Plot results curve
-          }
-           
-        # 2. specify cluster size and # clusters --> receive power vs. effect size
-          if (input$clusterRequest == "power vs. effect size"){
-            cluster.size <- reactive(as.numeric(input$cluster.size))
-            cluster.num  <- reactive(as.numeric(input$cluster.num))
-            effectSizes = seq(0, 2, by = 0.1)
-            plotTable <- data.frame(effectSizes, power = ctsClus_getpower_effectSize(EffectSizes))
-            # Plot results curve
-            # Add text that specifies "To achieve 80% power you need an effect size of ___"
-          }
-
-        # 3. specify power and # clusters -------> receive effect size vs. cluster size
-          if (input$clusterRequest == "effect size vs. cluster size"){
-            power        <- reactive(as.numeric(input$power))
-            cluster.num  <- reactive(as.numeric(input$cluster.num))
-            outTable <- data.frame(NULL)
-            for (clusterSize in clusterSizeRange){
-              outTable <- rbind(outTable, 
-                                data.frame(clusterSize = clusterSize,
-                                           effectSize = ctsClus_getEffectSize(power, clusterNum, clusterSize)))
-            }
-            # Plot results curve
-          }
-
-        # 4. specify power and cluster size ------> receive effect size vs. # clusters (discrete)
-          if (input$clusterRequest == "effect size vs. # clusters"){
-            cluster.size <- reactive(input$cluster.size)
-            power        <- reactive(input$power)
-            clusterNums <- 1:50
-            plotTable <- data.frame(clusterNum, effectSize = ctsClus_getEffectSize_clusterNum(1:50))
-            # Plot results curve
-          }
-      } else {
-        # 1. specify effect size, receive power vs. sample size
-        ctsIndiv_getPower(sampleSize, effectSize)
-        # 2. specify sample size, receive power vs. effect size
-        ctsIndiv_getEffectSize(sampleSize, power)
-        # 3. specify power, receive effect size vs. sample size
-        ctsIndiv_getSampleSize(effectSize, power)
-      }
-    
-      # Bring in parameters appropriate to--and run--power calculations for cts outcome
-    } else if (input$outcomeType == "binary"){
-      if (input$clusterDesign == T){
+    if (input$clusterDesign == T){
+      
+      # 1. specify power and effect size --> receive cluster size vs. # clusters (discrete)
+        # /!\ Minor note--could redo this sequence of if()s to be a switch()
+        if (input$clusterRequest == "cluster size vs. # clusters"){
+          power       <- reactive(as.numeric(input$power))
+          effect.size <- reactive(as.numeric(input$effect.size))
+          clusterNums = 1:50
           
-        # Run simulation with clustering according to specified parameters
-        input$runSim # NSM: This creates dependency (i.e. will rerun this code) every time the "run sim" button is clicked.
-                     #      The trick is to isolate this run from all other changes of parameters, until the user is ready 
-                     #        to run. I'm not quite sure how to do this but, somehow, this example does it: https://github.com/rstudio/shiny-examples/blob/master/060-retirement-simulation/server.r
-      } else {
-        # Run unclustered analysis of binary outcome
-      }
+          #plotTable <- data.frame(clusterNum = clusterNums, clusterSize = ctsClus_getClusterSize_clusterNum ( clusterNums ))
+            # XXX Not yet defined
+#             plotOut <- ggplot(plotTable, aes(x = clusterNum, y = clusterSize)) + geom_line(colour = "blue", size = 1) +
+#               ggtitle(paste("Cluster Size vs. # of Clusters\nto obtain at least", sprintf("%3.2f", power), "with effect size", sprintf("%3.2f", effect.size))) +
+#               xlab("# Clusters") + ylab("Cluster Size")
+        }
+         
+      # 2. specify cluster size and # clusters --> receive power vs. effect size
+        if (input$clusterRequest == "power vs. effect size"){
+          cluster.size <- reactive(as.numeric(input$cluster.size))
+          cluster.num  <- reactive(as.numeric(input$cluster.num))
+          effectSizes = seq(0, 2, by = 0.05)
+          
+          plotTable <- data.frame(effectSize = effectSizes, power = ctsClus_getpower_effectSize(effectSizes))
+          plotOut <- ggplot(plotTable, aes(x = effectSize, y = power)) + geom_line(colour = "blue", size = 1) +
+            ggtitle(paste("Power vs. Effect Size\nfor", cluster.num, "clusters of", cluster.size, "observations each")) +
+            xlab("Effect Size") + ylab("Power") + geom_hline(yintercept = 0.80)
+          # Add text that specifies "To achieve 80% power you need an effect size of ___"
+        }
+
+      # 3. specify power and # clusters --> receive effect size vs. cluster size
+        if (input$clusterRequest == "effect size vs. cluster size"){
+          power        <- reactive(as.numeric(input$power))
+          cluster.num  <- reactive(as.numeric(input$cluster.num))
+          clusterSizes <- seq(10, 200, by = 5)
+          
+          plotTable <- data.frame(clusterSize = clusterSizes, effectSize = ctsClus_getEffectSize_clusterSize(clusterSizes))
+          plotOut <- ggplot(plotTable, aes(x = clusterSize, y = effectSize)) + geom_line(colour = "blue", size = 1) +
+            ggtitle(paste("Effect Size vs. Cluster Size\nfor", cluster.num, "clusters, for power =", sprintf("%3.2f", power))) +
+            xlab("Cluster Size") + ylab("Effect Size")
+        }
+
+      # 4. specify power and cluster size --> receive effect size vs. # clusters (discrete)
+        if (input$clusterRequest == "effect size vs. # clusters"){
+          cluster.size <- reactive(input$cluster.size)
+          power        <- reactive(input$power)
+          clusterNums <- 1:50
+          plotTable <- data.frame(clusterNum = clusterNums, effectSize = ctsClus_getEffectSize_clusterNum(clusterNums))
+          plotOut <- ggplot(plotTable, aes(x = clusterNum, y = effectSize)) + geom_line(colour = "blue", size = 1) +
+            ggtitle(paste("Effect Size vs. # of Clusters\nfor", cluster.size, "obs per cluster, for power =", sprintf("%3.2f", power))) +
+            xlab("# of Clusters") + ylab("Effect Size")
+        }
+    } else { # i.e. if input$clusterDesign != T
+      cluster.num <- 1
+      
+      # 1. specify effect size, receive power vs. sample size
+        effect.size <- reactive(input$effect.size)
+        sampleSizes <- seq(10, 1000, by = 5)
+          
+#           plotTable <- data.frame(sampleSize = sampleSizes, power = ctsClus_getpower_clusterSize(sampleSizes))
+          # XXX Not yet developed
+#           plotOut <- ggplot(plotTable, aes(x = sampleSize, y = power)) + geom_line(colour = "blue", size = 1) +
+#             ggtitle(paste("Power vs. Sample Size\nfor effect size of", sprintf("%3.2f", effect.size))) +
+#             xlab("Effect Size") + ylab("Power") + geom_hline(yintercept = 0.80)
+
+      # 2. specify sample size, receive power vs. effect size
+        sample.size <- reactive(input$sample.size)
+        effectSizes = seq(0, 2, by = 0.05)
+      
+        plotTable <- data.frame(effectSize = effectSizes, power = ctsClus_getpower_effectSize(effectSizes))
+        plotOut <- ggplot(plotTable, aes(x = effectSize, y = power)) + geom_line(colour = "blue", size = 1) +
+          ggtitle(paste("Power vs. Sample Size\nfor effect size of", sample.size)) +
+          xlab("Effect Size") + ylab("Power") + geom_hline(yintercept = 0.80)
+
+      # 3. specify power, receive effect size vs. sample size
+        power        <- reactive(as.numeric(input$power))
+        sampleSizes <- seq(10, 1000, by = 100)
+        
+        plotTable <- data.frame(sampleSize = sampleSizes, effectSize = ctsClus_getEffectSize_clusterSize(sampleSizes))
+        plotOut <- ggplot(plotTable, aes(x = sampleSize, y = effectSize)) + geom_line(colour = "blue", size = 1) +
+          ggtitle(paste("Effect Size vs. Sample Size\nfor power = ", sprintf("%3.2f", power))) +
+          xlab("Sample Size") + ylab("Effect Size")
+
+    } # End of code pertaining to unclustered continuous outcome designs
+  
+  } else if (input$outcomeType == "binary"){ # Code for binary outcome designs
+    
+    if (input$clusterDesign == T){
+        
+      # Run simulation with clustering according to specified parameters
+      Results <- reactive({
+        # Isolate all of the following assignments to not allow them to run until the "Run Sim" button is clicked
+        # Great description of isolation: http://shiny.rstudio.com/articles/isolation.html
+        #         input$runSim # NSM: This creates dependency (i.e. will rerun this code) every time the "run sim" button is clicked.
+                   #      The trick is to isolate this run from all other changes of parameters, until the user is ready 
+                   #        to run. I'm not quite sure how to do this but, somehow, this example does it: https://github.com/rstudio/shiny-examples/blob/master/060-retirement-simulation/server.r
+      })
+
+    } else { # Code for unclustered designs
+      # Run unclustered analysis of binary outcome
     }
+  } # End of binary designs
     
   output$plot1 <- renderPlot({  
-    plot (Results(), type = "l", xlab = colnames(Results())[1], ylab = "Power")
-    # XXX Put a line on the graph at 0.8
+#     plot (Results(), type = "l", xlab = colnames(Results())[1], ylab = "Power")
+    plotOut
   })
   
   # Table output
-  output$table <- renderTable(Results())
+#   output$table <- renderTable(Results())
   
-  output$downloadData <- downloadHandler(
-    filename = "sim-results-out.csv",
-    content = function(file) {
-      write.csv(Results(), file, row.names = FALSE)
-    }
-  )
+#   output$downloadData <- downloadHandler(
+#     filename = "sim-results-out.csv",
+#     content = function(file) {
+#       write.csv(Results(), file, row.names = FALSE)
+#     }
+#   )
 
 })
--- ui.R
+++ ui.R
@@ -3,78 +3,76 @@
 library(shiny)
 library(shinyapps)
 shinyUI(fluidPage(
-  titlePanel("Simulation-Based Statistical Power Calculator"),
+  titlePanel("Experimental Design Power Calculator"), # Simulation-Based Statistical 
   wellPanel(
     fluidRow(
       column(3, 
         h4("Study Design"),
         radioButtons(inputId = "design", label = NULL,
-                     choices = c("RCT/Difference Between Groups"), # , "Single Population Estimate", "LQAS"
+                     choices = c("RCT/Difference Between Groups", "Single Population Estimate (inactive)", "LQAS (inactive)"), 
                      selected = NULL, inline = FALSE),
         checkboxInput("clusterDesign", "Clustered study design?", value = T)
       ),
       column(3,
         h4("Outcome Type"),
         radioButtons(inputId = "outcome", label = NULL,
-                     choices = c("Continuous"), selected = NULL, inline = FALSE) # "Binary", "Count",
+                     choices = c("Continuous", "Binary (inactive)", "Count (inactive)"), selected = NULL, inline = FALSE)
+      ),
+      column(3,
+        h4("Longitudinal Design (inactive)"),
+        checkboxInput("longDesign", "Longitudinal study design?", value = F),
+        conditionalPanel(
+          condition = "input.longDesign == true",
+          numericInput("long.followups", "Number of follow-ups", 1, min = 1, max = 10),
+          sliderInput("long.ICC", "ICC for longitudinal measures", min = 0.0, max = 1.0, step = 0.01, value = 0.25)
+        )
       )
-#       ,column(3,
-#         h4("Longitudinal Design"),
-#         checkboxInput("longDesign", "Longitudinal study design?", value = F),
-#         conditionalPanel(
-#           condition = "input.longDesign == true",
-#           numericInput("long.followups", "Number of follow-ups", 1, min = 1, max = 10),
-#           sliderInput("long.ICC", "ICC for longitudinal measures", min = 0.0, max = 1.0, step = 0.01, value = 0.25)
-#         )
-#       )
     )
   ),
-  br(), #, hr(), br(),
+  #br(), hr(), br(),
   fluidRow(
     column(3,
 #       wellPanel(
 #         p(actionButton("runSim", "Run simulation", icon("bolt"))) # look here for all icons - http://fontawesome.io/icons/
 #       ),
+      wellPanel(
+        h4("Power parameters"),
+        sliderInput("alpha", HTML("Alpha (&alpha;)"), 0.05, min = 0.01, max = 0.1, step = 0.01),
+#         numericInput("n.iter", "Number of Simulation Iterations", 10, min = 1)
+        numericInput("resid.var", "Residual variation", 1.0,  min = 0.01,   max = 10.0),
+        #tags$div(title = withMathJax("The equation for ICC is: $$\\frac{\\sigma^2_{Between}}{\\sigma^2_{Total}}$$"), # 
+        conditionalPanel(condition = "input.clusterDesign == true",
+          sliderInput("cluster.ICC", "", label = "Intra-Cluster Correlation (ICC)", min = 0, max = 1, step = 0.01, value = 0.25),
+          p("ICC is the fraction of the total individual variance that is attributable to between-cluster variance. An ICC=0
+            approximates an individually randomized controlled study. The larger the ICC is, the more important cluster
+            differences are in individual outcomes.")
+            #withMathJax("The equation for ICC is: $$\\frac{\\sigma^2_{Between}}{\\sigma^2_{Total}}$$")
+        )
+      ),
       conditionalPanel(condition = "input.clusterDesign == true",
-#         wellPanel(
-#           h4("Sample Sizes"),
-#           numericInput("cluster.size", "Number of clusters per arm (m)", 25, min = 1, max = 1000),
-#           numericInput("cluster.num",  "Number of units per cluster (n)", 40, min = 1)
-#         ),
         wellPanel(
           h4("Specify Request"),
-          selectInput("clusterRequest", "What curve are you requesting?",
+          selectInput("clusterRequest", "What relationship do you need to explore?",
                       c("cluster size vs. # clusters",
                         "power vs. effect size",
                         "effect size vs. cluster size",
                         "effect size vs. # clusters")),
           conditionalPanel(condition = "input.clusterRequest == 'cluster size vs. # clusters'",
             numericInput("power",       "Power",       0.8, min = 0.0, max = 1.0),
-            numericInput("effect.size", "Effect size", 0.5, min = 0,   max = 4),
+            numericInput("effect.size", "Effect size", 0.5, min = 0,   max = 4)
           ),
           conditionalPanel(condition = "input.clusterRequest == 'power vs. cluster size'",
             numericInput("cluster.size", "Effect size",        0.5, min = 0, max = 4),
-            numericInput("cluster.num",  "Number of clusters", 20,  min = 1, max = 100),
+            numericInput("cluster.num",  "Number of clusters", 20,  min = 1, max = 100)
           ),
           conditionalPanel(condition = "input.clusterRequest == 'effect size vs. cluster size'",
             numericInput("power",       "Power",              0.8, min = 0.0, max = 1.0),
-            numericInput("cluster.num", "Number of clusters", 20,  min = 1,   max = 100),
+            numericInput("cluster.num", "Number of clusters", 20,  min = 1,   max = 100)
           ),
           conditionalPanel(condition = "input.clusterRequest == 'effect size vs. # clusters'",
             numericInput("power",        "Power",                   0.8, min = 0.0, max = 1.0),
-            numericInput("cluster.size", "Sample size per cluster", 50,  min = 1,   max = 1000),
+            numericInput("cluster.size", "Sample size per cluster", 50,  min = 1,   max = 1000)
           )
-        ),
-        wellPanel(
-          h4("Power parameters"),
-          numericInput("resid.var", "Residual variation", 1.0,  min = 0.01,   max = 10.0),
-          #tags$div(title = withMathJax("The equation for ICC is: $$\\frac{\\sigma^2_{Between}}{\\sigma^2_{Total}}$$"), # 
-          sliderInput("cluster.ICC", "", label = "Intra-Cluster Correlation (ICC)", min = 0, max = 1, step = 0.01, value = 0.25),
-          p("ICC is the fraction of the total individual variance that is attributable to between-cluster variance. An ICC=0
-            approximates an individually randomized controlled study. The larger the ICC is, the more important cluster
-          differences are in individual outcomes.")
-            #withMathJax("The equation for ICC is: $$\\frac{\\sigma^2_{Between}}{\\sigma^2_{Total}}$$")
-          #)
         )
       ),
       conditionalPanel(condition = "input.clusterDesign == false",
@@ -82,7 +80,7 @@ shinyUI(fluidPage(
           h4("Sample Size"),
           numericInput("sample.size", "Sample size per arm", 500, min = 1, max = 10000)
         )
-      ),
+      )
 #       wellPanel(
 #         h4("Treatment Parameters"),
 #         sliderInput("baseline.prev", "Prevalence under control conditions",   min = 0, max = 1, step = 0.01, value = 0.8),
@@ -100,12 +98,7 @@ shinyUI(fluidPage(
 #           numericInput("or.vals.num",  "Number of values to test in this range", 5, min = 0, max = 100), 
 #           p("The odds ratio represents the treatment effect--relative to baseline--that you want to detect.") #, where values below zero represent a protective effect, and those above zero represent detrimental effects.
 #         )
-#       ),
-      wellPanel(
-        h4("Power Parameters"),
-        sliderInput("alpha", HTML("Alpha (&alpha;)"), 0.05, min = 0.01, max = 0.1, step = 0.01),
-#         numericInput("n.iter", "Number of Simulation Iterations", 10, min = 1)
-      )
+#       )
     ),
     column(6,
       plotOutput("plot1")
